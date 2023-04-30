727d0947191958915c7a151c67c69e6f01f02a7a
LeoMeloL <124972726+LeoMeloL@users.noreply.github.com>
2023-04-30 20:20:53 -0300

    Update Trabalho4.c

--- Trabalho4.c
+++ Trabalho4.c
@@ -2,35 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-void insertionSort(int arr[], int n) {
+void insertionSort( int arr[ ], int n ) {
     int i, key, j;
-    for (i = 1; i < n; i++) {
-        key = arr[i];
+    for ( i = 1; i < n; i++ ) {
+        key = arr[ i ];
         j = i - 1;
 
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
+        while ( j >= 0 && arr [j ] > key ) {
+            arr[ j + 1 ] = arr[ j ];
             j = j - 1;
         }
-        arr[j + 1] = key;
+        arr[ j + 1 ] = key;
     }
 }
 
-int* maxSubsequence(int* nums, int numsSize, int k, int* returnSize){
+int* maxSubsequence( int* nums, int numsSize, int k, int* returnSize ){
 
     int i, j, x = numsSize;
 
-    int *subsequence = (int*)malloc(k * sizeof(int));
+    int *subsequence = ( int* )malloc( k * sizeof( int ) );
 
-    int *final = (int*)malloc(x * sizeof(int));
+    int *final = ( int* )malloc( x * sizeof( int ) );
 
 
-    memcpy(final, nums, x * sizeof(int));
-    insertionSort(final, x);
+    memcpy( final, nums, x * sizeof( int ) );
+    insertionSort( final, x );
     
-    for (i = x, j = 0; j != k; i--){
+    for ( i = x, j = 0; j != k; i-- ){
 
-            subsequence[j++] = final[i - 1];
+            subsequence[ j++ ] = final[ i - 1 ];
     }
 
     *returnSize = k;
@@ -38,20 +38,20 @@ int* maxSubsequence(int* nums, int numsSize, int k, int* returnSize){
 }
 
 int main() {
-    int nums[] = {-1, 4, 2, -2};
+    int nums[ ] = { -1, 4, 2, -2 };
     int k = 3;
-    int size = sizeof(nums) / sizeof(nums[0]);
+    int size = sizeof( nums ) / sizeof( nums[ 0 ] );
     
-    int* subsequence = maxSubsequence(nums, size, k, &size);
+    int* subsequence = maxSubsequence( nums, size, k, &size );
     
     printf("Subsequencia de comprimento %d com a maior soma: ", k);
-    for (int i = 0; i < k; i++) {
-        printf("%d ", subsequence[k-i-1]);
+    for ( int i = 0; i < k; i++ ) {
+        printf("%d ", subsequence[ k-i-1 ]);
     }
     printf("\n");
     printf("\n");
 
-    free(subsequence);
+    free( subsequence );
     
     return 0;
 }
